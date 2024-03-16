dd7bc5ce1ae2bff26e8a4681edeaa835545a14a5
eizyc <yizhichen98@gmail.com>
2024-03-15 19:09:26 -0500

    fix[input]: support both controlled and uncontrolled for input component

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,12 +1,14 @@
-import { PropsWithChildren, InputHTMLAttributes, ReactElement, ChangeEvent, forwardRef } from 'react';
+import { PropsWithChildren, InputHTMLAttributes, ReactElement, ChangeEvent, forwardRef, useLayoutEffect, useState } from 'react';
 import classnames from 'classnames'
 import { STYLE_PREFIX } from "../../utils/const";
+import { useWatch } from '../../utils/hooks';
 
 export const prefixCls = `${STYLE_PREFIX}-input`
 
 type InputSize = 'lg' | 'md' |'sm'
-export interface InputProps extends Omit<InputHTMLAttributes<HTMLElement>, 'size'|'prefix'|'suffix' > {
+export interface InputProps extends Omit<InputHTMLAttributes<HTMLElement>, 'size'|'prefix'|'suffix'|'value' > {
     className?: string;
+    value?:string;
     disabled?: boolean;
     block?: boolean;
     size?: InputSize;
@@ -18,7 +20,19 @@ export interface InputProps extends Omit<InputHTMLAttributes<HTMLElement>, 'size
 }
 
 export const Input = forwardRef<HTMLInputElement, PropsWithChildren<InputProps>>((props, ref) => {
-    const { className, disabled, block, size, prefix, suffix, prepend, append, ...restProps } = props
+    const { className, disabled, block, size, prefix, suffix, prepend, append, defaultValue, onChange, value, ...restProps } = props
+
+    const [_value, setValue] = useState(() =>value??defaultValue??'')
+
+    useWatch(() => {
+      setValue(value??'');
+    }, [value])
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      setValue(value)
+      onChange?.(e)
+    };
 
     const classes = classnames(prefixCls, className, {
       [`${prefixCls}-size-${size}`]: size,
@@ -37,6 +51,8 @@ export const Input = forwardRef<HTMLInputElement, PropsWithChildren<InputProps>>
           ref={ref}
           className={`${prefixCls}-inner`}
           disabled={disabled}
+          value={_value}
+          onChange={onChangeHandler}
           {...restProps}
         />
         {suffix && <div className={`${prefixCls}-suffix`}>{suffix}</div>}
