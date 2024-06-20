4fe1be8a59eae29fa986c5bc9662e201ea185b77
eizyc <yizhichen98@gmail.com>
2024-06-19 23:56:53 -0500

    chore[umd]: bundle splitting the fortawesome package

--- webpack.umd.config.js
+++ webpack.umd.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 // It's a UMD configuration
 // Use tsconfig.json for ts-loader
 // Use .babelrc for babel-loader
 module.exports = {
-  entry: './src/index.tsx', // Entry
+  entry: {
+    'index':  './src/index.tsx',
+  }, // Entry
   output: {
     path: path.resolve(__dirname, 'dist/umd'),
-    filename: 'index.js',
+    filename: '[name].js',
     library: {
       name: 'EizyUI', // The name of library
       type: 'umd', 
@@ -39,4 +42,29 @@ module.exports = {
    'react-dom': 'ReactDOM',
    axios: 'Axios'
   },
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        fortawesome: {
+          test: /[\\/]node_modules[\\/](@fortawesome)[\\/]/,
+          name: 'fortawesome',
+          chunks: 'all',
+          reuseExistingChunk: true,
+          enforce: true,
+          priority: 20,
+        },
+        vendor: {
+          test: /[\\/]node_modules[\\/]/i,
+          name: 'vendor',
+          chunks: 'all',
+          reuseExistingChunk: true,
+          enforce: true,
+          priority: 10,
+        }
+      }
+    }
+  }
 };
